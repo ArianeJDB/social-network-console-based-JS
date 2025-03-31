06dc9177bfe80ff9a2403f472817e2e84451e96b
ArianeJDB <ariane.jurado@gmail.com>
2025-03-31 16:27:12 +0200

    Prints message and how long ago was it posted when it is in plural, singular or less than a minute ago

--- src/Printer.js
+++ src/Printer.js
@@ -11,12 +11,19 @@ module.exports = {
     } = dependencies
   
     function print(messages) {
-      const currentTime = getCurrentTimestamp();
-      const messageTime = messages[0].timestamp;
-      const diffInMs = currentTime - messageTime;
-      const diffInMinutes = Math.floor(diffInMs / (60 * 1000));
-
-        consolelog(`${messages[0].message} (${diffInMinutes} minutes ago)`)
+        const currentTime = getCurrentTimestamp();
+        const messageTime = messages[0].timestamp;
+        const diffInMs = currentTime - messageTime;
+        const diffInMinutes = Math.floor(diffInMs / (60 * 1000));
+        let text;
+        if (diffInMinutes < 1) {
+          text = "less than a minute ago"
+        } else if (diffInMinutes === 1) {
+          text = `${diffInMinutes} minute ago`
+        } else {
+          text = `${diffInMinutes} minutes ago`
+        }
+        consolelog(`${messages[0].message} (${text})`)
         return messages
 
     }
--- tests/printer.test.js
+++ tests/printer.test.js
@@ -9,14 +9,19 @@ const printer = Printer.create({
   getCurrentTimestamp: getCurrentTimestampMock
 })
 
-test("Prints message and how long ago was it posted", () => {
+test.each([
+  ["when it is more than 1 minute", 5, " minutes ago"],
+  ["when it is 1 minute", 1, " minute ago"],
+  ["when it is less than minute", '', "less than a minute ago"]
+])("Prints message and how long ago was it posted when it is %s", (_, minutes, text) => {
     const messages = [{message: "message", timestamp: 123456789}]
-    const currentTimestamp = 123456789 + (5 * 60 * 1000);
+    const currentTimestamp = 123456789 + (minutes * 60 * 1000);
     when(getCurrentTimestampMock)
     .calledWith()
     .mockReturnValue(currentTimestamp);
     
     printer.print(messages)
 
-    expect(consolelogMock).toHaveBeenCalledWith("message (5 minutes ago)")
+    expect(consolelogMock).toHaveBeenCalledWith(`message (${minutes}${text})`)
 })
+
