173f0e502ec5797bce50244d46039b2d1233ec30
ArianeJDB <ariane.jurado@gmail.com>
2025-04-01 09:25:55 +0200

    add another userFollowing to test more than one

--- tests/wall.test.js
+++ tests/wall.test.js
@@ -18,6 +18,7 @@ const wall = Wall.create({
 test("returns both own and following messages", () => {
     const user = "user"
     const userFollowing = "userFollowing"
+    const anotherUserFollowing = "anotherUserFollowing"
     const messagesFromUserFollowing = [
         {message: "message", timestamp: 123456789},
         {message: "another message", timestamp: 123456790}
@@ -25,22 +26,29 @@ test("returns both own and following messages", () => {
     const ownMessages = [
         {message: "other message", timestamp: 123456789}
     ]
+    const messagesFromAnotherUserFollowing = [
+        {message: "yet other message", timestamp: 123456790}
+    ]
     when(followingRetriever.get)
         .calledWith(user)
-        .mockReturnValue([userFollowing])
+        .mockReturnValue([userFollowing, anotherUserFollowing])
     when(retriever.get)
         .calledWith(user)
         .mockReturnValue(ownMessages)
     when(retriever.get)
         .calledWith(userFollowing)
         .mockReturnValue(messagesFromUserFollowing)
+    when(retriever.get)
+        .calledWith(anotherUserFollowing)
+        .mockReturnValue(messagesFromAnotherUserFollowing)
 
     const result = wall.process(user)
 
     const expectedMessages = [
         {message: "other message", timestamp: 123456789},
         {message: "message", timestamp: 123456789},
-        {message: "another message", timestamp: 123456790}
+        {message: "another message", timestamp: 123456790},
+        {message: "yet other message", timestamp: 123456790}
     ]
     expect(result).toMatchObject(expectedMessages)
 
